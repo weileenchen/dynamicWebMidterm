a20877594a6c187b03dd3ad120561e007b477bb0
weileenchen <wc1627@nyu.edu>
2021-11-01 21:25:33 -0400

    touch up

--- src/components/APIKeyNutrition.js
+++
@@ -1,5 +0,0 @@
-function APIKeyNutrition() {
-  return `ZzK+ZTtw+X/lttbuTL2mcA==K1QsunDdVwlNv2sY`;
-}
-
-export default APIKeyNutrition;
--- src/components/APIKeyRecipes.js
+++
@@ -1,5 +0,0 @@
-function APIKeyRecipes() {
-  return `ba9f32427244490bbc91b1fafced6c7f`;
-}
-
-export default APIKeyRecipes;
